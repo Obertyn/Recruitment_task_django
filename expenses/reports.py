9e6015886f0f36afd97d6b305fa7551b8e55d517
Obertyn <sikoramateusz1999@gmail.com>
2022-11-07 09:39:43 +0100

    update

--- expenses/reports.py
+++ expenses/reports.py
@@ -1,8 +1,8 @@
 from collections import OrderedDict
-
-from django.db.models import Sum, Value
+from django.db.models import Sum, Value, Count
 from django.db.models.functions import Coalesce
-
+from django.db.models import CharField
+from .models import Expense, Category
 
 def summary_per_category(queryset):
     return OrderedDict(sorted(
@@ -14,3 +14,85 @@ def summary_per_category(queryset):
         .values_list('category_name', 's')
     ))
 
+
+def summary_all(queryset):
+    summary = queryset.aggregate(suma=Sum('amount'))
+    print(summary)
+    try:
+        summary = round(summary['suma'], 2)
+    except:  # obsługa błędu występującego gdy lista jest pusta
+        summary = 0
+    return summary
+
+
+def summary_per_month(queryset):
+    list1 = queryset.values('date__year', 'date__month').annotate(s=Sum('amount'))
+    list1 = list(list1)
+    summary_month = [[""]*2 for i in range(len(list1))]
+    for j in range(len(list1)):
+        summary_month[j][0] = str(list1[j]['date__month'])+"/"+str(list1[j]['date__year'])
+    for j in range(len(list1)):
+        summary_month[j][1] = list1[j]['s']
+    return summary_month
+
+
+def summary_per_year(queryset):
+    return OrderedDict(sorted(
+        queryset
+        .annotate(aaaaa=Coalesce('date__year', Value('-'), output_field=CharField()))
+        .order_by()
+        .values('aaaaa')
+        .annotate(s=Sum('amount'))
+        .values_list('aaaaa', 's')
+    ))
+
+
+def data_for_category_list():
+
+    data = Expense.objects.all()
+    data = (data
+            .annotate(categoryid=Coalesce('category__id', Value('-'), output_field=CharField()))
+            .order_by('category__name')
+            .values('categoryid')
+            .annotate(s=Count('category'))
+            .values_list('categoryid', 's'))
+    count_without_count0 = [0] * len(data)
+    id_without_count0 = [0] * len(data)
+    for i in range(len(data)):
+        count_without_count0[i] = data[i][1]
+    for i in range(len(data)):
+        id_without_count0[i] = data[i][0]
+
+    data = Category.objects.all()
+    data = (data.values('name'))
+    data = list(data)
+    # name_list i id_list są potrzebne do wyświetlenia danych w kolumnach "name" i "actions"
+    name_list = [0] * len(data)
+    for i in range(len(data)):
+        name_list[i] = data[i]['name']
+    data = Category.objects.all()
+    data = (data.values('id'))
+    data = list(data)
+    id_list = [0] * len(data)
+    for i in range(len(data)):
+        id_list[i] = data[i]['id']
+
+    count_list = []
+    x = 0
+    # w bazie z wydatkami nie znajdują się kategorię z przedmiotami, których nigdy nie zakupiono (np. transport)
+    # powoduje to, że lista z sumą wydatków jest krótsza - nie zawiera wartości z zerem, w związku z tym,
+    # powiększam listę z sumą wydatków, o kategorie z ilością zakupionych przedmiotów równą 0
+    for i in range(len(id_list)):
+        if id_list[i] in id_without_count0:
+            count_list.append(count_without_count0[x])
+            x = x + 1
+        else:
+            count_list.append(0)
+    # łączę wszystkie 3 listy ze sobą
+    data_for_category_list = [[None] * 3 for i in range(len(name_list))]
+    for i in range(len(name_list)):
+        data_for_category_list[i][0] = name_list[i]
+        data_for_category_list[i][1] = count_list[i]
+        data_for_category_list[i][2] = id_list[i]
+
+    return data_for_category_list
--- expenses/views.py
+++ expenses/views.py
@@ -2,15 +2,25 @@ from django.views.generic.list import ListView
 
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
-from .reports import summary_per_category
+from .reports import summary_per_category, summary_all, summary_per_year, summary_per_month, data_for_category_list
+import datetime
 
 
 class ExpenseListView(ListView):
     model = Expense
     paginate_by = 5
-
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
+
+        def get_start_date(self):
+            start_date = self.request.GET.get('start_date')
+            return start_date
+
+        def get_end_date(self):
+            start_date = self.request.GET.get('end_date')
+            return start_date
+        start_date = get_start_date(self)
+        end_date = get_end_date(self)
 
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
@@ -18,13 +28,74 @@ class ExpenseListView(ListView):
             if name:
                 queryset = queryset.filter(name__icontains=name)
 
+        start_date2 = ''
+        end_date2 = ''
+        if(start_date):
+            for i in ('%d-%m-%Y', '%d.%m.%Y', '%d/%m/%Y'):
+                try:
+                    start_date2 = datetime.datetime.strptime(start_date, i).date()
+                except:  # obsługa błędu polegającego na tym, że użytkownik wpisze coś co nie jest datą
+                    pass  # w takim przypadku wpisana wartość nie zostanie wzięta pod uwagę, przy wyszukiwaniu
+                          # a text input zostanie wyzerowany, żeby użytkownik wiedział że popełnił błąd
+        if(end_date):
+            for i in ('%d-%m-%Y', '%d.%m.%Y', '%d/%m/%Y'):
+                try:
+                    end_date2 = datetime.datetime.strptime(end_date, i).date()
+                except:
+                    pass
+
+        if (start_date2 != "" and end_date2 != ""):
+            queryset = queryset.filter(date__range=[start_date2, end_date2])
+        elif(start_date2 != ""):
+            queryset = queryset.filter(date__range=[start_date2, "9000-01-01"])
+            end_date = ""
+        elif(end_date2 != ""):
+            queryset = queryset.filter(date__range=["1000-01-01", end_date2])
+            start_date = ""
+        else:
+            end_date = ""
+            start_date = ""
+
+        if end_date == None:
+            end_date = ""
+
+        if start_date == None:
+            start_date = ""
+
+        def get_sorting(self):
+            select = self.request.GET.get('select')
+            return select
+
+        select = get_sorting(self)
+
+        if (select == "sort_category_a"):
+            queryset = queryset.order_by('category')
+        if (select == "sort_category_d"):
+            queryset = queryset.order_by('-category')
+        if (select == "sort_date_a"):
+            queryset = queryset.order_by('date')
+        if (select == "sort_date_d"):
+            queryset = queryset.order_by('-date')
+
         return super().get_context_data(
+            sort_category_a="sort_category_a",
             form=form,
+            end_date=end_date,
+            start_date=start_date,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
-            **kwargs)
+            summary_all=summary_all(queryset),
+            summary_per_year=summary_per_year(queryset),
+            summary_per_month=summary_per_month(queryset),
+            **kwargs,)
+
 
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+
+        return super().get_context_data(
+            data_for_category_list=data_for_category_list,
+            **kwargs,)
